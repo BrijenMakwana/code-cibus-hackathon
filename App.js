eecedb105cfd47451d9a28e83ac8286a63257459
Brijen Makwana <brijenmakwana@192.168.1.5>
2022-10-15 17:40:56 +0530

    navigation

--- App.js
+++ App.js
@@ -4,9 +4,27 @@ import HomeScreen from "./screens/HomeScreen";
 import MenuScreen from "./screens/MenuScreen";
 import VendorDashboardScreen from "./screens/VendorDashboardScreen";
 import VendorSignInScreen from "./screens/VendorSignInScreen";
+import QRScanScreen from "./screens/QRScanScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <MenuScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="VendorSignIn" component={VendorSignInScreen} />
+        <Stack.Screen
+          name="VendorDashboard"
+          component={VendorDashboardScreen}
+        />
+        <Stack.Screen name="QRScan" component={QRScanScreen} />
+        <Stack.Screen name="Menu" component={MenuScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
